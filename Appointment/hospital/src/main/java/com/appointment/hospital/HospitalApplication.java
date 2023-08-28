1e336f81f798c6d1808f644c32bd8028af66b4e4
ganeshpogul2000 <pogulganesh1@gmail.com>
2023-08-28 06:08:37 +0000

    final commit to application

--- Appointment/hospital/src/main/java/com/appointment/hospital/HospitalApplication.java
+++ Appointment/hospital/src/main/java/com/appointment/hospital/HospitalApplication.java
@@ -2,8 +2,10 @@ package com.appointment.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HospitalApplication {
 
 	public static void main(String[] args) {
